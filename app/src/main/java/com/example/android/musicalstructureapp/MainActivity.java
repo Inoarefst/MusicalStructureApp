7d623fe1fc2b6cc6213e0726ee5f07bb4bd50264
Inoarefst <33956323+Inoarefst@users.noreply.github.com>
2018-03-08 18:10:53 +0200

    fixed issues

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -7,41 +7,44 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView rockTextView;
+    TextView hipHopTextView;
+    TextView partyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Set onClickListener on the rock list text view
-        TextView rockTextView = findViewById(R.id.tv_rock_list);
-        rockTextView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent rockIntent = new Intent(MainActivity.this, RockActivity.class);
-                startActivity(rockIntent);
-            }
-        });
-
-        //Set onClickListener on the hip hop list text view
-        TextView hipHopTextView = findViewById(R.id.tv_hip_hop_list);
-        hipHopTextView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent hipHopIntent = new Intent(MainActivity.this, HipHopActivity.class);
-                startActivity(hipHopIntent);
-            }
-        });
-
-        //Set onClickListener on the party list text view
-        TextView partyTextView = findViewById(R.id.tv_party_list);
-        partyTextView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent partyIntent = new Intent(MainActivity.this, PartyActivity.class);
-                startActivity(partyIntent);
-            }
-        });
+        rockTextView = findViewById(R.id.tv_rock_list);
+        hipHopTextView = findViewById(R.id.tv_hip_hop_list);
+        partyTextView = findViewById(R.id.tv_party_list);
+        rockTextView.setOnClickListener(this);
+        hipHopTextView.setOnClickListener(this);
+        partyTextView.setOnClickListener(this);
     }
+
+    //set a click listener for each text view.
+    public void onClick(View v) {
+        switch (v.getId()){
+                case R.id.tv_rock_list:
+        //start rock activity.
+        Intent rockIntent = new Intent(this, RockActivity.class);
+        this.startActivity(rockIntent);
+        break;
+        //start hip hop activity.
+                case R.id.tv_hip_hop_list:
+        Intent hipHopIntent = new Intent(this, HipHopActivity.class);
+        this.startActivity(hipHopIntent);
+        break;
+        //start party activity.
+                case R.id.tv_party_list:
+        Intent partyIntent = new Intent(this, PartyActivity.class);
+        this.startActivity(partyIntent);
+        break;
+            }
 }
+}
+
+
+

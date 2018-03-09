32e12b012e27793b8bd3b8785957d693bafaae1f
Inoarefst <33956323+Inoarefst@users.noreply.github.com>
2018-03-09 08:46:43 +0200

    fixed issues

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -26,24 +25,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     //set a click listener for each text view.
     public void onClick(View v) {
-        switch (v.getId()){
-                case R.id.tv_rock_list:
-        //start rock activity.
-        Intent rockIntent = new Intent(this, RockActivity.class);
-        this.startActivity(rockIntent);
-        break;
-        //start hip hop activity.
-                case R.id.tv_hip_hop_list:
-        Intent hipHopIntent = new Intent(this, HipHopActivity.class);
-        this.startActivity(hipHopIntent);
-        break;
-        //start party activity.
-                case R.id.tv_party_list:
-        Intent partyIntent = new Intent(this, PartyActivity.class);
-        this.startActivity(partyIntent);
-        break;
-            }
-}
+        switch (v.getId()) {
+            case R.id.tv_rock_list:
+                //start rock activity.
+                Intent rockIntent = new Intent(this, RockActivity.class);
+                this.startActivity(rockIntent);
+                break;
+            //start hip hop activity.
+            case R.id.tv_hip_hop_list:
+                Intent hipHopIntent = new Intent(this, HipHopActivity.class);
+                this.startActivity(hipHopIntent);
+                break;
+            //start party activity.
+            case R.id.tv_party_list:
+                Intent partyIntent = new Intent(this, PartyActivity.class);
+                this.startActivity(partyIntent);
+                break;
+        }
+    }
 }
 
 
